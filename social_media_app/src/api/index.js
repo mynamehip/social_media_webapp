dc751acb2fd5a45d17974c51adecb4bb0bb93aa3
unknown <myname8102@gmail.com>
2024-07-19 11:41:06 +0700

    fix reponsive

--- social_media_app/src/api/index.js
+++ social_media_app/src/api/index.js
@@ -8,5 +8,5 @@ export const token = () => {
   return JSON.parse(tokenData).result;
 };
 
-export const hostURL = "https://localhost:7293";
-export const hostAPI = axios.create({ baseURL: "https://localhost:7293" });
+export const hostURL = "https://scmwaAPI.somee.com";
+export const hostAPI = axios.create({ baseURL: "https://scmwaAPI.somee.com" });
